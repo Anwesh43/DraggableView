d70c09dbdf39beab9b4c1767c2eb13936aaacdc1
Anwesh43 <anweshthecool0@gmail.com>
2016-12-20 04:50:35 +0530

    Adding DraggableButton in library and demo

--- draggableviews/src/main/java/com/anwesome/ui/draggableviews/DraggableButton.java
+++ draggableviews/src/main/java/com/anwesome/ui/draggableviews/DraggableButton.java
@@ -1,7 +1,30 @@
 package com.anwesome.ui.draggableviews;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.Button;
+import android.widget.ImageView;
+
 /**
  * Created by anweshmishra on 20/12/16.
  */
-public class DraggableButton {
+public class DraggableButton extends Button {
+    private GestureDetector gestureDetector;
+
+    public DraggableButton(Context context, AttributeSet attrs) {
+        super(context,attrs);
+        gestureDetector = new GestureDetector(new DraggableGestureDetector(this));
+    }
+    public DraggableButton(Context context) {
+        super(context);
+        gestureDetector = new GestureDetector(new DraggableGestureDetector(this));
+    }
+    public void setOnClickListener(ImageView.OnClickListener clickListener) {
+        gestureDetector = new GestureDetector(new DraggableGestureDetector(this,clickListener));
+    }
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
 }
