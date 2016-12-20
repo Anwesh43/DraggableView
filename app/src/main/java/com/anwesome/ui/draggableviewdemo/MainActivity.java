36f0d53723d5160ce87f46b4bca854ea727172b4
Anwesh43 <anweshthecool0@gmail.com>
2016-12-20 14:33:31 +0530

    Testing onclick of draggable views

--- app/src/main/java/com/anwesome/ui/draggableviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/draggableviewdemo/MainActivity.java
@@ -2,6 +2,11 @@ package com.anwesome.ui.draggableviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.imv)
+    public void onImvClick(View view) {
+        Toast.makeText(this,"Clicked on imageview",Toast.LENGTH_SHORT).show();
+    }
+    @OnClick(R.id.btn)
+    public void onBtnClick(View view) {
+        Toast.makeText(this,"Clicked On button",Toast.LENGTH_SHORT).show();
     }
 }
